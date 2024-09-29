32503453f788a1c75cab6088c6c027fd014ba1ed
ShareAbraham <sharethomas81@gmail.com>
2024-09-29 15:04:39 -0500

    Add pizza place script with variables, types, and conditional logic

---
+++ pizzaplacehomework.js
@@ -0,0 +1,15 @@
+const pizzaPlace = "Slice of Heaven";
+const numberOfToppings = 10;
+
+console.log(pizzaPlace);
+console.log(typeof pizzaPlace);
+console.log(numberOfToppings);
+console.log(typeof numberOfToppings);
+
+console.log(`Welcome to ${pizzaPlace}. We offer ${numberOfToppings} toppings`);
+
+if (numberOfToppings < 10) {
+  console.log("Quality, not quantity.");
+} else {
+  console.log("A whole lot of pizza.");
+}
